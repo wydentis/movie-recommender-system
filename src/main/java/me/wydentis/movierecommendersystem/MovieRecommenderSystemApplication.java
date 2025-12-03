a20e75d16ac196d93454530756a296bc0473685c
wydentis <sapetp54@gmail.com>
2025-12-03 22:44:14 +0300

    Init commit

---
+++ src/main/java/me/wydentis/movierecommendersystem/MovieRecommenderSystemApplication.java
@@ -0,0 +1,13 @@
+package me.wydentis.movierecommendersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRecommenderSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieRecommenderSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/wydentis/movierecommendersystem/MovieRecommenderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package me.wydentis.movierecommendersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRecommenderSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
