ef9f2cc2c92872661d1057634d76ff59f43eb7e8
wydentis <sapetp54@gmail.com>
2025-12-03 22:53:11 +0300

    complete lesson 4

--- src/main/java/me/wydentis/movierecommendersystem/ContentBasedFilter.java
+++ src/main/java/me/wydentis/movierecommendersystem/ContentBasedFilter.java
@@ -1,5 +1,10 @@
 package me.wydentis.movierecommendersystem;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
 public class ContentBasedFilter implements Filter {
     public String[] getRecommendations(String movie) {
         return new String[] {"Happy Feet", "Ice Age", "Shark Tale"};
